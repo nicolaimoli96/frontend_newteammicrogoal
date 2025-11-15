03cd77b19036861a3dbf27bcb3fbaf39dbd4d55d
Nicola Imoli <imoli.nicola@gmail.com>
2025-11-15 13:25:37 +0000

    Fix: Remove unused useMemo import

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
 import WeeklyProgressCircle from '../components/WeeklyProgressCircle';
 import ProgressCircle from '../components/ProgressCircle';
